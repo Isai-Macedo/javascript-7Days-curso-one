13b269af15e073caa7b0aed276726da99f9103cb
Isai-Macedo <isaimexiko@gmail.com>
2025-09-08 10:11:30 -0600

    Actualizacion 20250908

--- reto-4/reto-4.js
+++ reto-4/reto-4.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener('DOMContentLoaded', function () {
   const contenido = document.getElementById('contenido');
 
@@ -9,3 +10,20 @@ document.addEventListener('DOMContentLoaded', function () {
     <a href="../reto-5/reto-5.html">Siguiente (Reto 5)</a>
   `;
 });
+*/
+const numeroAdivinacion = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let intento = "";
+let acerto = false;
+
+for(let contador = 0; contador < 3; contador++){    
+    intento = prompt("Intenta adivinar el número del 0 al 10:");
+    if(intento == numeroAdivinacion){
+        alert("¡Felicidades, acertaste! El número era ${numeroAdivinacion}.");
+        acerto = true;
+        break;
+    }
+    alert("¡Incorrecto!");
+}
+if(!acerto){
+    alert("Desafortunadamente, no acertaste. ¡El número era ${numeroAdivinacion}!");
+}
